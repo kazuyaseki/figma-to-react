4703aa4bcc0de20f14c2bb5efa3730ed01a4d8d4
kazuyaseki <track613armory@gmail.com>
2021-04-26 23:03:14 +0900

    setup webpack to run react

---
+++ src/ui.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+
+ReactDom.render(<div></div>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -4,27 +4,37 @@ const path = require('path')
 
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
+
+  // This is necessary because Figma's 'eval' works differently than normal eval
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    code: './src/code.ts'
+    ui: './src/ui.tsx', // The entry point for your UI code
+    code: './src/code.ts' // The entry point for your plugin code
   },
 
   module: {
     rules: [
+      // Converts TypeScript code to JavaScript
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+
+      // Enables including CSS by doing "import './file.css'" in your TypeScript code
       { test: /\.css$/, use: ['style-loader', { loader: 'css-loader' }] },
+
+      // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
       { test: /\.(png|jpg|gif|webp|svg)$/, loader: 'url-loader' }
     ]
   },
 
+  // Webpack tries these extensions for you if you omit the extension like "import './file'"
   resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
 
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist') // Compile into a folder called "dist"
   },
 
+  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/ui.html',
@@ -32,6 +42,6 @@ module.exports = (env, argv) => ({
       inlineSource: '.(js)$',
       chunks: ['ui']
     }),
-    new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+    new HtmlWebpackInlineSourcePlugin()
   ]
 })
